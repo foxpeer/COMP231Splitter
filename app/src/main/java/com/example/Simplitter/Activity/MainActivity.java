3bcd63b67f64efcca147bebd64a2b78027a66177
Liping Wu <foxpeer@gmail.com>
2020-11-07 15:38:53 -0500

    Simplitter-login and register clean for quick test

--- app/src/main/java/com/example/Simplitter/Activity/MainActivity.java
+++ app/src/main/java/com/example/Simplitter/Activity/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -26,52 +27,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userViewModel= ViewModelProviders.of(this).get(UserViewModel.class);
-
-        etEmail =(EditText)findViewById(R.id.editText_email);
-        etPassword =(EditText) findViewById(R.id.editText_password);
-        validation=(TextView)findViewById(R.id.textViewValidation);
+        userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
+        etEmail = (EditText) findViewById(R.id.editText_email);
+        etPassword = (EditText) findViewById(R.id.editText_password);
+        validation = (TextView) findViewById(R.id.textViewValidation);
     }
 
     public void LoginClick(View view) {
         validation.setText("");
-        try {
+        //for quick test only
+       String email = etEmail.getText().toString();
+        Toast.makeText(getApplicationContext(), "Welcome back " +email, Toast.LENGTH_SHORT).show();
+        Log.d("Login Check", "MainActivity::LoginClick: Welcome back ");
+
+/*
+       try {
             String email = etEmail.getText().toString();
             String password = etPassword.getText().toString();
-            userViewModel.getUserByEmail(email).observe(MainActivity.this,optionalN->{
-                optionalN.ifPresent(user->{
-                    if(user.getPassword().equals(password)){
-                        //validation.setText("Welcome "+n.getFirstname()+" "+n.getLastname());
-                        userPref = getSharedPreferences("user",MODE_PRIVATE);
-                        editor=userPref.edit();
+            userViewModel.getUserByEmail(email).observe(MainActivity.this, optionalN -> {
+                optionalN.ifPresent(user -> {
+                    if (user.getPassword().equals(password)) {
+                        validation.setText("Welcome " + user.getFirstname() + " " + user.getLastname());
+                        userPref = getSharedPreferences("user", MODE_PRIVATE);
+                        editor = userPref.edit();
                         editor.putString("email", user.getEmail());
-                        editor.putString("firstName",user.getFirstname());
-                        editor.putString("lastName",user.getLastname());
+                        editor.putString("firstName", user.getFirstname());
+                        editor.putString("lastName", user.getLastname());
                         editor.commit();
+                        Toast.makeText(getApplicationContext(), "Welcome back "+ email +"!", Toast.LENGTH_SHORT).show();
+                        Log.d("Login Check", "MainActivity::LoginClick: Welcome back "+ email +"! ");
 
-                   /* add in the future for new activity
-                   //    Intent intent = new Intent(this, NavActivity.class);
+                   *//* add in the future for new activity added
+                        Intent intent = new Intent(this, NavActivity.class);
                         intent.putExtra("nurseId", nurseId);
                         intent.putExtra("firstName", n.getFirstname());
                         intent.putExtra("lastName", n.getLastname());
-                        startActivity(intent);*/
-                    }
-                    else {
+                        startActivity(intent);*//*
+
+                    } else {
                         validation.setText("Failed to verify your credential, please try again");
                     }
                 });
             });
-        }
-        catch (Exception ex){
+        } catch (Exception ex) {
             validation.setText("Failed to verify your credential, please try again");
-        }
+        }*/
 
     }
+
+    //Register Click Button Clicked
     public void RegisterClick(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
-        Toast.makeText(getApplicationContext(),"Register as a new user", Toast.LENGTH_SHORT).show();
-
+        Toast.makeText(getApplicationContext(), "Register as a new user", Toast.LENGTH_SHORT).show();
     }
-
 }
+
